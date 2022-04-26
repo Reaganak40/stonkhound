7d9edec81a64f7ba20ecac1b16ba5d3dafd1686f
Reagan <reagan.kelley@wsu.edu>
2022-04-26 16:38:33 -0700

    predicts stock prices

--- Stonkhound/Stonkhound.py
+++ Stonkhound/Stonkhound.py
@@ -1,14 +1,25 @@
 from format_data import *
+from stocks_learning_algorithm import *
+from sklearn.neural_network import MLPClassifier
 
 def main():
     print("=========================================")
     print("         WELCOME TO STONKHOUND!")
     print("=========================================")
 
-    data = get_dataset()    
-    
-    for dp in data:
-        print(dp)
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+
+    avg = 0
+    num_trials = 10000
+    for i in range(num_trials):
+        avg += run_diagnostics(clf, get_dataset())
+    avg /= num_trials
+    print("average accuracy: {} from {} trials".format(round(avg, 3), num_trials))
+
+def run_diagnostics(clf, data):
+    X_train, y_train, X_test, y_test = train_test_split(data, 0.9)
+    clf.fit(X_train, y_train.ravel())
+    return clf.score(X_test, y_test)
 
 if __name__ == "__main__":
     main()
---
+++ Stonkhound/stocks_learning_algorithm.py
@@ -0,0 +1,35 @@
+
+import random
+import numpy as np
+
+def train_test_split(dataset, train_size):
+    num_training = int(train_size * len(dataset))
+
+    train = []
+    test = []
+
+    # Get random datapoints to put in training data
+    for i in range(num_training):
+        random_dp = random.randint(0, (len(dataset)-1))
+        train.append(dataset.pop(random_dp))
+    
+    # Put unchosen datapoints in testing data
+    for dp in dataset:
+        test.append(dp)
+
+    y_train_ = []
+    y_test_ = []
+    for i in range(len(train)):
+        # removes the label from the features and puts in in y
+        y_train_.append(train[i].pop(len(train[i])-1))
+
+    for i in range(len(test)):
+        # removes the label from the features and puts in in y
+        y_test_.append(test[i].pop(len(test[i])-1))
+    
+    X_train = np.array(train)
+    y_train = np.array(y_train_)
+    X_test = np.array(test)
+    y_test = np.array(y_test_)
+    
+    return np.reshape(X_train, (-1, 8)), np.reshape(y_train, (-1, 1)), np.reshape(X_test, (-1, 8)), np.reshape(y_test, (-1, 1))
